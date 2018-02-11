7df6254a7ec329cd2fe7cc2061ee7da034009f7a
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-11 22:33:27 +0330

    some bugs fixed

--- src/lottery.js
+++ src/lottery.js
@@ -2,3 +2,6 @@ import web3 from './web3';
 
 const address = '0xc0DF362a3ff6B60871Ad84116D02472C7DfCDbA4';
 const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[{"name":"","type":"address"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"entryPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+
+export default new web3.eth.Contract(abi,address);
